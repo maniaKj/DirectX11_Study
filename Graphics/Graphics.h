f0997cd476b7a656d3ab17cb58cc22434a3495c9
Jinpyo_K <39121365+maniaKj@users.noreply.github.com>
2019-03-13 18:06:13 +0900

    upload

---
+++ Graphics/Graphics.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "AdapterReader.h"
+#include "Shaders.h"
+#include "Vertex.h"
+
+class Graphics {
+public:
+	bool Initialize(HWND hwnd, int width, int height);
+	void RenderFrame();
+private:
+	bool InitializeDirectX(HWND hwnd, int width, int height);
+	bool InitializeShaders();
+	bool InitializeScene();
+
+	Microsoft::WRL::ComPtr<ID3D11Device> device; //디바이스 인터페이스 : 기능 지원 점검과 자원 할당에 쓰임
+	Microsoft::WRL::ComPtr<ID3D11DeviceContext> deviceContext; //디바이스 컨텍스트 인터페이스 : 렌더 대상을 설정하고 자원을 그래픽 파이프라인에 묶고 Gpu가 수행할 렌더링 명령들을 지시하는데 쓰인다.
+	Microsoft::WRL::ComPtr<IDXGISwapChain> swapchain; //프론트 버퍼 백 버퍼 바꿔치기
+	Microsoft::WRL::ComPtr<ID3D11RenderTargetView> renderTargetView;
+
+	VertexShader vertexshader;
+	PixelShader pixelshader;
+
+	Microsoft::WRL::ComPtr<ID3D11Buffer> vertexBuffer;
+	Microsoft::WRL::ComPtr<ID3D11Buffer> vertexBuffer2;
+
+	Microsoft::WRL::ComPtr<ID3D11DepthStencilView> depthStencilView;
+	Microsoft::WRL::ComPtr<ID3D11Texture2D> depthStencilBuffer;
+	Microsoft::WRL::ComPtr<ID3D11DepthStencilState> depthStencilState;
+
+	Microsoft::WRL::ComPtr<ID3D11RasterizerState> rasterizerState;
+};
---
+++ sample.cpp
@@ -0,0 +1,26 @@
+#include "ErrorLogger.h"
+#include "Engine.h"
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_  HINSTANCE hPrevInstance,
+	_In_ LPWSTR lpCmdLine,
+	_In_ int nCmdShow) 
+{
+	/*HRESULT hr = S_OK;
+	if (SUCCEEDED(hr)) {
+		MessageBoxA(NULL, "SUCCESS", "SUCCESS", NULL);
+	}
+
+	if (FAILED(hr)) {
+		ErrorLogger::Log(E_INVALIDARG, "Test Message");
+	}*/
+
+	Engine engine;
+	engine.Initialize(hInstance, "title", "class", 800, 600);
+	while (engine.ProcessMessage() == true) {
+		engine.Update();
+		engine.RenderFrame();
+	}
+	
+	return 0;
+}
